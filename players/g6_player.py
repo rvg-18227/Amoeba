c5478835a142982ecd9383a2b5c57b3a2cc5538e
SkrighYZ <zhang.yipeng@columbia.edu>
2022-12-06 21:19:56 -0500

    fix bug when rows not large enough to do organize

--- players/g6_player.py
+++ players/g6_player.py
@@ -123,7 +123,7 @@ class Player:
             retract_list, expand_list = self.init_organize(
                 amoeba_map, current_percept.periphery, current_percept.bacteria)
             amoeba_loc = np.stack(np.where(amoeba_map == 1)).T.astype(int)
-            if amoeba_loc[:, 1].max() - amoeba_loc[:, 1].min() <= 4 \
+            if amoeba_loc[:, 1].max() - amoeba_loc[:, 1].min() <= 3 \
                 or min(len(retract_list), len(expand_list)) == 0:
                 info = 1
                 stage = 2
