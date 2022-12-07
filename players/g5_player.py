6b5b37ea86b564b244750e3f43319194550a7131
r1p71d3 <dz2428@columbia.edu>
2022-12-07 03:22:04 -0500

    code cleanup

--- players/g5_player.py
+++ players/g5_player.py
@@ -20,10 +20,9 @@ import random as rnd
 MAP_DIM = 100
 MAX_BASE_LEN = min(MAP_DIM, 100)
 TOOTH_SPACING = 1       # 1 best
-SHIFTING_FREQ = 6       # 6 best for high metabolisms, 4 better for low
+SHIFTING_FREQ = 8       # 6 best for high metabolisms, 4 better for low
 SIZE_MULTIPLIER = 4     # 4 best for density = 0.1 metabolism = 0.1
 MOVING_TYPE = 'center'  # 'center' best for low metabolisms - 'center_teeth_first' better for high
-TWO_RAKE = True
 
 # Best configs so far #
 # m = 0.1; A = 5; s = 2 -> 1 6 4 'center' -> 202 moves
@@ -136,7 +135,6 @@ def iter_from_middle(lst):
 # ********* BYTE INFO ******** #
 
 class MaxVals(Enum):
-    #is_rake = 2
     x_val = 100
     tooth_shift = 2
 
@@ -190,21 +188,6 @@ class Player:
                 precomp_dir (str): Directory path to store/load pre-computation
         """
 
-        # precomp_path = os.path.join(precomp_dir, "{}.pkl".format(map_path))
-
-        # # precompute check
-        # if os.path.isfile(precomp_path):
-        #     # Getting back the objects:
-        #     with open(precomp_path, "rb") as f:
-        #         self.obj0, self.obj1, self.obj2 = pickle.load(f)
-        # else:
-        #     # Compute objects to store
-        #     self.obj0, self.obj1, self.obj2 = _
-
-        #     # Dump the objects
-        #     with open(precomp_path, 'wb') as f:
-        #         pickle.dump([self.obj0, self.obj1, self.obj2], f)
-
         self.rng = rng
         self.logger = logger
         self.metabolism = metabolism
@@ -220,81 +203,21 @@ class Player:
         self.num_available_moves: int = None
         self.map_state: npt.NDArray = None
 
-    def generate_tooth_formation(self, amoeba_size: int) -> npt.NDArray:
-        formation = np.zeros((MAP_DIM, MAP_DIM), dtype=np.int8)
-        center_x = MAP_DIM // 2
-        center_y = MAP_DIM // 2
-        spacing = TOOTH_SPACING + 1
-
-        # find the number of complete 5-cell modules
-        complete_modules = amoeba_size // 5
-        # find the number of unfinished sections
-        additional_sections = amoeba_size % 5 // 2
-        # find whether there is an odd cell
-        reserve = amoeba_size % 5 % 2
-
-        base_len = min(complete_modules * 2 + additional_sections, MAX_BASE_LEN)
-        # teeth_len = min(complete_modules, base_len//2)
-        # teeth_len = min(complete_modules, base_len // spacing)
-        teeth_len = base_len // spacing
-        # reserve = max(reserve, amoeba_size-(base_len*2 + teeth_len))
-        start_y = center_y - base_len // 2
-
-        # add the 2-cell-wide base and teeth
-        for y in range(start_y, start_y + base_len):
-            formation[center_x, y%100] = 1
-            formation[center_x - 1, y%100] = 1
-            if y%100 % spacing == 0:
-                formation[center_x + 1, y%100] = 1
-
-        # add the teeth
-        # start_modules = start_y  # +(additional_sections+1)//2
-        # for y in range(start_modules, start_y + teeth_len * spacing):
-        #     # if (y - start_modules) % spacing == 0:
-        #     if y%100 % spacing == 0:
-        #         formation[center_x + 1, y%100] = 1
-
-        # # add the "reserve" cell at the back
-        # for i in range(reserve):
-        #     row = i//base_len
-        #     col = i % base_len
-        #     formation[center_x-2-row, start_y+col] = 1
-
-        # show_amoeba_map(formation)
-        return formation
-
     def generate_tworake_formation(self, amoeba_size: int, curr_x: int, shift: int) -> npt.NDArray:
         formation = np.zeros((MAP_DIM, MAP_DIM), dtype=np.int8)
-        center_x = MAP_DIM // 2
         center_y = MAP_DIM // 2
         spacing = TOOTH_SPACING + 1
-        # if amoeba_size > 300 and (abs(curr_x - 50) < 3 or abs(curr_x - 100) < 3):
 
         # PREVENT RAKES FROM COLLIDING
         d50 = lambda x: abs(x - (50 * round(x / 50)))
-        # print(amoeba_size)
         if amoeba_size > 350 and d50(curr_x) < 3:
             curr_x = ((50 * round(curr_x / 50)) + 3) % 100
-            teeth = False
-        else:
-            teeth = True
 
         # ADD FIRST RAKE
-        # print('gtf x: ', curr_x)
-        # find the number of complete 5-cell modules
         complete_modules = amoeba_size // 5
-        # find the number of unfinished sections
         additional_sections = amoeba_size % 5 // 2
-        # find whether there is an odd cell
-        reserve = amoeba_size % 5 % 2
-
         base_len = min(complete_modules * 2 + additional_sections, MAX_BASE_LEN)
-        # teeth_len = min(complete_modules, base_len//2)
-        # teeth_len = min(complete_modules, base_len // spacing)
-        teeth_len = base_len // spacing
-        # reserve = max(reserve, amoeba_size-(base_len*2 + teeth_len))
         start_y = center_y - base_len // 2
-        # start_x = (center_x + offset_x)%100
         start_x = curr_x
 
         # add the 2-cell-wide base and teeth
@@ -311,7 +234,6 @@ class Player:
         cells_used = (formation == 1).sum()
         available = amoeba_size - cells_used
 
-        # bar_length = min(100, available)
         if curr_x < 50:
             bar_length = min(100, available)
         else:
@@ -323,20 +245,11 @@ class Player:
         # ADD THE SECOND RAKE
         cells_used = (formation == 1).sum()
         available = amoeba_size - cells_used
-
         complete_modules = available // 5
-        # find the number of unfinished sections
         additional_sections = available % 5 // 2
-        # find whether there is an odd cell
-        reserve = available % 5 % 2
 
         base_len = min(complete_modules * 2 + additional_sections, MAX_BASE_LEN)
-        # teeth_len = min(complete_modules, base_len//2)
-        # teeth_len = min(complete_modules, base_len // spacing)
-        teeth_len = base_len // spacing
-        # reserve = max(reserve, amoeba_size-(base_len*2 + teeth_len))
         start_y = center_y - base_len // 2
-        # start_x = (center_x + offset_x)%100
 
         start_x = (99 - curr_x) % 100
         for y in range(start_y, start_y + base_len):
@@ -345,46 +258,8 @@ class Player:
             if y % 100 % spacing == shift ^ 1:
                 formation[(start_x - 1) % 100, y % 100] = 1
 
-        # DO SOMETHING WITH EXCESS CELLS
-        cells_used = (formation == 1).sum()
-        available = amoeba_size - cells_used
-        # cube_side = math.ceil((available ** 0.5))
-
-        # for y in iter_from_middle(list(range(0, 50)) + list(range(51, 100))):
-        #     for x in iter_from_middle(range(0, 100)):
-        #     # for x in range(center_x-cube_side//2, center_x-cube_side//2+cube_side):
-        #         if available <= 0:
-        #             break
-        #         formation[x, y] = 1
-        #         available -= 1
-        #     if available <= 0:
-        #         break
-
-        # for x in iter_from_middle(range(0, 100)):
-        #     for y in iter_from_middle(list(range(0, 50)) + list(range(51, 100))):
-        #     # for x in range(center_x-cube_side//2, center_x-cube_side//2+cube_side):
-        #         if available <= 0:
-        #             break
-        #         formation[x, y] = 1
-        #         available -= 1
-        #     if available <= 0:
-        #         break
-
-
         # show_amoeba_map(formation)
         return formation
-
-    # sort potential retracts based on the number of neighbors (less members -> higher priority)
-    # def sort_retracts(self, potential_retracts):
-    #     ranked_cells = []
-    #     for x, y in potential_retracts:
-    #         neighbors = [((x-1) % 100, y), ((x+1) % 100, y), (x, (y-1) % 100), (x, (y+1) % 100)]
-    #         score = 0
-    #         for neighbor in neighbors:
-    #             if self.amoeba_map[neighbor] == 1:
-    #                 score += 1
-    #         ranked_cells.append(((x, y), score))
-    #     return [cell for cell, score in sorted(ranked_cells, key=lambda t: t[1])]
 
     def get_retracts_neighbors(self, potential_retracts):
         ranked_cells_dict = {}
@@ -398,10 +273,10 @@ class Player:
         return ranked_cells_dict
 
     def sort_retracts(self, retracts, ranked_cells_dict):
-        if self.mem.x_val >= 50:
-            retracts = sorted(retracts, key=lambda r: (ranked_cells_dict[r], -abs(r[0]-50)), reverse=True)
+        if self.mem.x_val > 50:
+            retracts = sorted(retracts, key=lambda r: (ranked_cells_dict[r], abs(r[0]-50)), reverse=True)
         else:
-            retracts = sorted(retracts, key=lambda r: (ranked_cells_dict[r], abs(r[0] - 50)), reverse=True)
+            retracts = sorted(retracts, key=lambda r: (ranked_cells_dict[r], -abs(r[0] - 50)), reverse=True)
         return retracts
 
     def get_valid_neighbors(self, cell):
@@ -418,7 +293,7 @@ class Player:
         neighbors = [((x - 1) % 100, y), ((x + 1) % 100, y), (x, (y - 1) % 100), (x, (y + 1) % 100)]
         return neighbors
 
-    # copied from G2
+    # approach inspired by G2
     def get_morph_moves(self, desired_amoeba: npt.NDArray) -> Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
         """ Function which takes a starting amoeba state and a desired amoeba state and generates a set of retracts and extends
             to morph the amoeba shape towards the desired shape.
@@ -432,7 +307,6 @@ class Player:
         potential_extends = [p for p in list(set(desired_points).difference(set(current_points))) if
                              p in self.extendable_cells]
 
-        # potential_retracts.sort(key=lambda pos: pos[1])
         if MOVING_TYPE == 'top_down':
             potential_extends.sort(key=lambda pos: pos[1])
         elif MOVING_TYPE == 'top_down_teeth_first':
@@ -442,32 +316,14 @@ class Player:
         elif MOVING_TYPE == 'center_teeth_first':
             potential_extends.sort(key=lambda pos: (-pos[0], abs(50-pos[1])))
 
-        # Loop through potential extends, searching for a matching retract
         retracts = []
         extends = []
-        change = True   # tracks whether anything was moved to prevent endless loop
-        # potential_extends = potential_extends[::-1]
-        # while potential_extends and potential_retracts and change and len(retracts) < self.num_available_moves:
-        #     change = False
-        #     # for potential_extend in potential_extends:
-        #     #     for potential_retract in potential_retracts:
-        #     for potential_retract in potential_retracts:
-        #         # for potential_extend in potential_extends:
-        #         if self.check_move(retracts + [potential_retract], extends + [potential_extends[-1]]):
-        #             # matching retract found, add the extend and retract to our lists
-        #             retracts.append(potential_retract)
-        #             potential_retracts.remove(potential_retract)
-        #             # extends.append(potential_extend)
-        #             extends.append(potential_extends.pop())
-        #             # potential_extends.remove(potential_extend)
-        #             change = True
-        #             break
         ranked_cells_dict = self.get_retracts_neighbors(potential_retracts)
-        # potential_retracts.sort(key=lambda r: (ranked_cells_dict[r], -r[0]), reverse=True)
-        # potential_retracts = self.sort_retracts(potential_retracts)
         potential_retracts = self.sort_retracts(potential_retracts, ranked_cells_dict)
 
-        possible_moves = min(len(potential_retracts), len(potential_extends), self.num_available_moves)
+        max_turns = math.ceil(self.current_size * min(self.metabolism, 0.5))
+        possible_moves = min(len(potential_retracts), len(potential_extends), max_turns)
+
         potential_extends.reverse()
 
         for _ in range(possible_moves):
@@ -478,15 +334,10 @@ class Player:
             for neighbor in self.get_neighbors(next_ret):
                 if neighbor in ranked_cells_dict:
                     ranked_cells_dict[neighbor] -= 1
-            # potential_retracts.sort(key=lambda r: ranked_cells_dict[r], reverse=True)
-            # potential_retracts.sort(key=lambda r: (ranked_cells_dict[r], -r[0]), reverse=True)
+
             potential_retracts = self.sort_retracts(potential_retracts, ranked_cells_dict)
 
-        # retracts = potential_retracts[:possible_moves]
-        # extends = potential_extends[:possible_moves]
-
         while not self.check_move(retracts, extends):
-            # retracts = binary_search(retracts, lambda r: self.check_move(r, extends[:len(r)]))
             bad_retract = binary_search_item(retracts, lambda r: self.check_move(r, extends[:len(r)]))
 
             for neighbor in self.get_neighbors(bad_retract):
@@ -495,18 +346,12 @@ class Player:
 
             retracts.remove(bad_retract)
             extends.pop()
-
-            # potential_retracts.sort(key=lambda r: ranked_cells_dict[r], reverse=True)
-            # potential_retracts.sort(key=lambda r: (ranked_cells_dict[r], -r[0]), reverse=True)
             potential_retracts = self.sort_retracts(potential_retracts, ranked_cells_dict)
 
             if potential_retracts and potential_extends:
                 retracts.append(potential_retracts.pop())
                 extends.append(potential_extends.pop())
 
-        # show_amoeba_map(self.amoeba_map, retracts, extends)
-        # truncate to account for smaller metabolism
-        # print(self.check_move(retracts, extends))
         return retracts, extends
 
     # adapted from amoeba game code
@@ -554,7 +399,7 @@ class Player:
 
         return (amoeba == check).all()
 
-    # copied from G2
+    # adapted from G2
     def store_current_percept(self, current_percept: AmoebaState) -> None:
         self.current_size = current_percept.current_size
         self.amoeba_map = current_percept.amoeba_map
@@ -592,58 +437,31 @@ class Player:
 
         while len(retracts) == 0 and len(moves) == 0:
             if mem.tooth_shift == 0:
-                offset_x = mem.x_val - MAP_DIM//2 - 1
                 x_val = mem.x_val - 1
-                # offset_x = mem.x_val - MAP_DIM // 2
             else:
                 x_val = mem.x_val
-                offset_x = mem.x_val - MAP_DIM//2
 
             offset_y = 0 if (mem.x_val % (SHIFTING_FREQ*2)) < SHIFTING_FREQ else -1
 
-            if not TWO_RAKE:
-                if SIZE_MULTIPLIER == 0 or self.current_size > 250:
-                    target_formation = self.generate_tooth_formation(self.current_size)
-                # elif SIZE_MULTIPLIER < 0:
-                #     target_size = ((self.goal_size // 4 + (-SIZE_MULTIPLIER) * ((100 + offset_x) % 100)) + self.current_size) // 2
-                #     target_formation = self.generate_tooth_formation(target_size)
-                # elif self.current_size >= 250:
-                #     target_formation = self.generate_tworake_formation(self.current_size, mem.x_val)
-                else:
-                    target_size = self.goal_size // 4 + SIZE_MULTIPLIER * ((100 + offset_x) % 100)  # calculate the desired size with regard
-                    if target_size//self.current_size < 0.8:
-                        target_size = self.current_size
-                    target_formation = self.generate_tooth_formation(target_size)
-
-                # target_formation = self.generate_tooth_formation(last_percept.current_size)
-                target_formation = np.roll(target_formation, offset_x, 0)
-                target_formation = np.roll(target_formation, offset_y, 1)
-            else:
-                target_size = self.goal_size // 4 + SIZE_MULTIPLIER * (
-                            (100 + offset_x) % 100)  # calculate the desired size with regard
-                # if target_size // self.current_size < 0.8:
-                #     target_size = self.current_size
-
-                target_formation = self.generate_tworake_formation(self.current_size, x_val, offset_y + 1)
-                # target_formation = self.generate_tworake_formation(target_size, x_val, offset_y + 1)
+            target_formation = self.generate_tworake_formation(self.current_size, x_val, offset_y + 1)
 
             diff = np.count_nonzero(target_formation & self.amoeba_map != target_formation)
             if diff/self.current_size <= 0.15 and diff <= 30:
                 retracts, moves = [], []
             else:
                 retracts, moves = self.get_morph_moves(target_formation)
-                # if len(retracts)/self.num_available_moves < 0.2:
+                # max_turns = math.ceil(self.current_size * min(self.metabolism, 0.5))
+                # max_turns = min(len(self.retractable_cells), max_turns)
+                # if len(retracts) / max_turns < 0.1 and len(retracts) < 2:
                 #     retracts, moves = [], []
 
             if len(retracts) == 0 and len(moves) == 0:
-            # if random.random() < 0.5 or (len(retracts) == 0 and len(moves) == 0):
                 if mem.tooth_shift == 1:
                     mem.x_val = (mem.x_val + 1) % 100
                     if mem.x_val % SHIFTING_FREQ == 0:
                         mem.tooth_shift = 0
                 else:
                     mem.tooth_shift = 1
-                    # mem.x_val = (mem.x_val + 1) % 100
 
         info = mem.get_byte()
         return retracts, moves, info
@@ -675,33 +493,6 @@ class Player:
             return True
         return False
 
-    def find_movable_cells(self, retract, periphery, amoeba_map, bacteria, mini):
-        movable = []
-        new_periphery = list(set(periphery).difference(set(retract)))
-        for i, j in new_periphery:
-            nbr = self.find_movable_neighbor(i, j, amoeba_map, bacteria)
-            for x, y in nbr:
-                if (x, y) not in movable:
-                    movable.append((x, y))
-
-        movable += retract
-
-        return movable[:mini]
-
-    # def find_movable_neighbor(self, x, y, amoeba_map, bacteria):
-    #     out = []
-    #     if (x, y) not in bacteria:
-    #         if amoeba_map[x][(y - 1) % 100] == 0:
-    #             out.append((x, (y - 1) % 100))
-    #         if amoeba_map[x][(y + 1) % 100] == 0:
-    #             out.append((x, (y + 1) % 100))
-    #         if amoeba_map[(x - 1) % 100][y] == 0:
-    #             out.append(((x - 1) % 100, y))
-    #         if amoeba_map[(x + 1) % 100][y] == 0:
-    #             out.append(((x + 1) % 100, y))
-    #
-    #     return out
-
     def find_movable_neighbor(self, x, y):
         out = []
         if self.map_state[x][(y - 1) % MAP_DIM] < 1:
@@ -714,4 +505,3 @@ class Player:
             out.append(((x + 1) % MAP_DIM, y))
 
         return out
-
